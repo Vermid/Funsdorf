36c434704a352edc5699689ff0b7d07530d19000
CraZzyShaDoW <almirbalija@gmail.com>
2016-12-05 18:45:40 +0100

    ItemSystem
    added the scripts for the ItemSystem

---
+++ Game/Funsdorf_Project/Assets/Game_Stuff/Scripts/Items/ItemSystem/ItemStructure.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ItemStructure
+{
+    private string itemName;    //Name vom Item
+    private int itemRarity;  //Seltenheit von 0 bis 5
+    private int itemType;  //ItemType 0-3
+
+    ///////////////////////
+    //GET AND SET METHODS//
+    ///////////////////////
+
+    public void SetItemName(string sItemName)
+    {
+        itemName = sItemName;
+    }
+
+    public string GetItemName() { return itemName; }
+
+    public void SetItemRarity(int nRarity)
+    {
+        itemRarity = nRarity;
+    }
+
+    public int GetItemRarity() { return itemRarity; }
+
+    public void SetItemType(int nItemType)
+    {
+        itemType = nItemType;
+    }
+
+    public int GetItemType() { return itemType; }
+}
+
